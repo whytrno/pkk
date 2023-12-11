56ab0fa9a5ef8836de5eedc146d9c760dcdcb7a3
Wahyu Triono <whytrno@gmail.com>
2023-12-11 08:43:13 +0700

    Add whatsapp number in footer

--- app/(main)/footer.jsx
+++ app/(main)/footer.jsx
@@ -1,10 +1,11 @@
 const Footer = () => {
     return (
         <div className="bg-primary rounded-t-xl text-white px-20 py-14">
-            <div className="flex justify-center">
+            <div className="flex justify-center flex-col items-center gap-5">
                 <p className="text-center w-1/3">Jl. Gerilya Timur No.26, Sokabaru, Berkoh, Kec. Purwokerto Sel.,
                     Kabupaten
                     Banyumas, Jawa Tengah 53146</p>
+                <a href="https://wa.me/6281542871112">Whatsapp: +62 815-4287-1112</a>
             </div>
         </div>
     )
